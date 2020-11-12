ef33a932a94368c278a97befff8415dbb9055292
Resy-24 <72025700+Resy-24@users.noreply.github.com>
2020-11-11 23:56:43 -0600

    Actividad 2: crear un proyecto en la terminal

---
+++ FI/POO/POOP6V2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FI.POO;
+
+/**
+ *
+ * @author Resendiz Cruz Rodrigo Daniel
+ */
+public class POOP6V2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Yo soy la clase POOP6V2 ewe");
+    }
+}
